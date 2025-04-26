740867dfe728733e6dd8fd911bc6ed123ed5146c
Luizzzin <luizhenribarbodias@gmail.com>
2025-04-26 12:25:20 -0300

    feat: up

--- Escola/src/main/java/com/escola/Escola/controller/AlunoController.java
+++ Escola/src/main/java/com/escola/Escola/controller/AlunoController.java
@@ -1,6 +1,10 @@
 package com.escola.Escola.controller;
 
+import com.escola.Escola.model.Aluno;
 import com.escola.Escola.service.AlunoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 public class AlunoController {
     private final AlunoService _alunoService;
@@ -8,4 +12,30 @@ public class AlunoController {
     public AlunoController(AlunoService _alunoService) {
         this._alunoService = _alunoService;
     }
+
+    @GetMapping
+    public List<Aluno> listar() {
+        return _alunoService.listarTodos();
+    }
+    @PostMapping
+    public Aluno criar (@RequestBody Aluno aluno){
+        return _alunoService.salvar(aluno);
+    }
+    @GetMapping("/{id}")
+    public Aluno atualizar(@PathVariable Long id, @RequestBody Aluno novoaluno){
+        Aluno alunoExistente = _alunoService.buscarPorId(id);
+        if (alunoExistente == null)return null ;
+
+        alunoExistente.setNomeAluno(novoaluno.getNomeAluno());
+        alunoExistente.setEmailAluno(novoaluno.getEmailAluno());
+        alunoExistente.setTelefoneAluno(novoaluno.getTelefoneAluno());
+        return _alunoService.salvar(alunoExistente);
+    }
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        _alunoService.deletar(id);
+    }
+
+
+
 }
