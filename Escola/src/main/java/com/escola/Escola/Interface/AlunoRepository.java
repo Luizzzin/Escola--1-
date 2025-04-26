cd22e39c00f8620fe3a43d2c0e4dfd84c317f294
Luizzzin <luizhenribarbodias@gmail.com>
2025-04-26 10:33:20 -0300

    feat: repository

--- Escola/src/main/java/com/escola/Escola/Interface/AlunoRepository.java
+++ Escola/src/main/java/com/escola/Escola/Interface/AlunoRepository.java
@@ -1,4 +1,7 @@
 package com.escola.Escola.Interface;
 
-public interface AlunoRepository {
+import com.escola.Escola.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 }
