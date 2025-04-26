f3175dc937aac0088275cdc73cc6037a528b0471
Luizzzin <luizhenribarbodias@gmail.com>
2025-04-26 15:11:03 -0300

    up

--- Escola/src/main/java/com/escola/Escola/controller/AlunoController.java
+++ Escola/src/main/java/com/escola/Escola/controller/AlunoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/alunos")
 public class AlunoController {
     private final AlunoService _alunoService;
