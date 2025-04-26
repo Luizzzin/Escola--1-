164465250c2466f85883e6c7253cf397cfaba7ee
Luizzzin <luizhenribarbodias@gmail.com>
2025-04-26 11:43:03 -0300

    feat: aluno controller

--- Escola/src/main/java/com/escola/Escola/controller/AlunoController.java
+++ Escola/src/main/java/com/escola/Escola/controller/AlunoController.java
@@ -1,4 +1,11 @@
 package com.escola.Escola.controller;
 
+import com.escola.Escola.service.AlunoService;
+
 public class AlunoController {
+    private final AlunoService _alunoService;
+
+    public AlunoController(AlunoService _alunoService) {
+        this._alunoService = _alunoService;
+    }
 }
--- Escola/src/main/java/com/escola/Escola/service/AlunoService.java
+++ Escola/src/main/java/com/escola/Escola/service/AlunoService.java
@@ -1,4 +1,29 @@
 package com.escola.Escola.service;
 
+import com.escola.Escola.Interface.AlunoRepository;
+import com.escola.Escola.model.Aluno;
+
+import java.util.List;
+
 public class AlunoService {
+    private final AlunoRepository _repository;
+
+    public AlunoService(AlunoRepository repository) {
+        this._repository = repository;
+    }
+
+    public List<Aluno> listarTodos(){
+        return _repository.findAll();
+    }
+
+    public Aluno salvar(Aluno aluno){
+        return _repository.save(aluno);
+    }
+
+    public void deletar(Long id){
+        _repository.deleteById(id);
+    }
+    public Aluno buscarPorId(Long id){
+        return _repository.findById(id).orElse(null);
+    }
 }
